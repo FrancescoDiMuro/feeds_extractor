738e3080fb41a667c7f0cedb4b4eaf9b19f66422
Francesco Dimuro <francesco.dimuro@adecco.it>
2024-06-30 00:11:51 +0200

    feat: add factories

---
+++ database/factories/InstagramSourceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Feed;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InstagramSource>
+ */
+class InstagramSourceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique->name,
+            'fan_count' => $this->faker->numberBetween(20000, 2000000),
+            'feed_id' => $this->faker->numberBetween(1, Feed::count())
+        ];
+    }
+}
