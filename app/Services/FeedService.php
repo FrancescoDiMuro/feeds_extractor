4333b9558f553f9a7039095b6937a5d1d87115db
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2024-06-30 18:53:19 +0200

    refactor: refactor code and add comments for a better readibility

--- app/Services/FeedService.php
+++ app/Services/FeedService.php
@@ -6,7 +6,7 @@ use App\Models\Feed;
 
 class FeedService {
 
-    public function retrieveFeed(int $feedId, ?string $only, ?int $includePosts) {
+    public function retrieveFeed(int $feedId, ?string $only, ?int $includePosts): array {
         
         // Retrieve the feed from the db
         $feed = Feed::find($feedId);
@@ -14,28 +14,52 @@ class FeedService {
         // If the feed has been found
         if(!empty($feed)) {
             
+            // Retrieve the specified source, or all if not specified
+            // If a value different by "instagram" or "tiktok" is used
+            // (and so, even an empty value), the default value is used
             $sources = match($only) {
                 'instagram' => $feed->instagramSource,
                 'tiktok' => $feed->tiktokSource,
                 default => [$feed->instagramSource, $feed->tiktokSource]
             };
 
+            // If the user specified a number of posts
+            // Setting 0 to $includePosts makes the condition false,
+            // since empty($includePosts) == true
             if(!empty($includePosts)) {
-                $feedPosts = $feed->posts()->limit($includePosts)->get();
+                
+                // Obtaining a specific number ($includePosts) of posts
+                $feedPosts = $feed->posts()->orderBy('id')->limit($includePosts)->get();
             }
 
+            // Prepare the data to return
+            $feedData = [$feed->id, $feed->name];
+            
+            // If at least a source has been found
+            if(!empty($sources)) {
+                
+                // If more than a source has been found,
+                // then loop through them and extract the information;
+                // else, just assign the source
+                $feedSources = is_array($sources) 
+                                ? array_map(
+                                    function ($source) {
+                                        return $source->toArray();
+                                    }, 
+                                    $sources
+                                )
+                                : [$sources->toArray()];
+            }
+
+            // Return the data
             return [
-                'feed' => [$feed->id, $feed->name],
-                'feedSources' => $sources,
+                'feed' => $feedData,
+                'feedSources' => $feedSources ?? [],
                 'feedPosts' => $feedPosts ?? []
             ];
         }
 
-        // Feed: {feedId}
-        // Sources:
-        // ...
-        // Posts:
-        // ...
+        return [];
     }
 
 }
